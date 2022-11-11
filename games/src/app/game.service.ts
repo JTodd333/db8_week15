d86f198da900507384c551d2196ae042ea611135
Jesse Todd <jessetodd@rocketmortgage.com>
2022-11-11 10:47:29 -0500

    slight change to lab 3

--- games/src/app/game.service.ts
+++ games/src/app/game.service.ts
@@ -9,7 +9,8 @@ export class GameService {
   TheList: Game[] = [
     { title: 'Game1', year: 2020, console: false},
     { title: 'Game2', year: 2022, console: true},
-    { title: 'Cult of the Lamb', year: 2022, console: true}
+    { title: 'Cult of the Lamb', year: 2022, console: true},
+    { title: 'Team Fortress Classic', year: 1999, console: false}
   ];
   constructor() { }
 
